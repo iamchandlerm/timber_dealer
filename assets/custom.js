e7ea173af2c370a837a8fa09c755cddf32a46bb2
MichaelAngelo-Khel-ops <michael1118salonga@gmail.com>
2026-06-15 12:28:22 +0800

    save

---
+++ assets/custom.js
@@ -0,0 +1,37 @@
+const initializeSwiper = ( container = document , line = false ) => {
+    
+        container
+        .querySelectorAll('.mySwiper')
+        .forEach((slider) => {
+
+           
+                
+            if(slider.swiper) {
+                return
+            }
+
+
+            new Swiper('.mySwiper', {
+                loop: true,
+                pagination: {
+                    clickable: true,
+                    el: ".swiper-pagination",
+                    type: "progressbar",
+
+                },
+                navigation: {
+                  nextEl: '.swiper-button-next',
+                  prevEl: '.swiper-button-prev',
+                },
+              });
+        });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+        initializeSwiper(document);
+        
+})
+
+document.addEventListener('shopify:section:load', (event) => {
+        initializeSwiper(event?.target);
+});
